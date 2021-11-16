9729b910767a55822fa7f3fea34af5b5739ede11
Anas Abid <abid.anas3@gmail.com>
2021-11-17 01:00:19 +0500

    whatever

---
+++ app.js
@@ -0,0 +1,25 @@
+// alert("HELLO CLASS....!")
+
+//1)
+// var date = new Date()
+// function foo1(){
+//     console.log(date)
+// }
+// foo1()
+
+//2)
+// var firstName = prompt("ENTER YOUR FIRST NAME?")
+// var lastName = prompt("ENTER YOUR LAST NAME?")
+// function foo2(){
+// document.write(firstName +" "+ lastName + " WELCOME TO OUR NEW WEBSITE!!!! ")
+// }
+// foo2()
+
+//3)
+var num1 = +prompt("ENTER THE FIRST NUMBER?")
+var num2 = +prompt("ENTER THE SECOND NUMBER?")
+var finalValue = num1+num2
+function foo3(){
+    alert(" FINAL VALUE = "+finalValue)
+}
+foo3()
